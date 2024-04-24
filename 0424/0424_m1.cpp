1021d11d5bf9181459107d70e20dc98d65252338
JungInYeong <167291569+JungInYeong@users.noreply.github.com>
2024-04-24 15:49:54 +0900

    실습1

---
+++ 0424/0424_m1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void Add(int a, int b)
+{
+	cout << "ÇÕ : " << a + b << endl;
+}
+
+void Sub(int a, int b)
+{
+	cout << "Â÷ : "  << a - b << endl;
+}
+
+void Mul(int a, int b)
+{
+	cout << "°ö¼À : " << a * b << endl;
+}
+
+void Divide(int a, int b)
+{
+	cout << "³ª´°¼À : " << (float)a / b << endl;
+}
+
+int main()
+{
+	int num1, num2;
+	cout << "¼ýÀÚ¸¦ ÀÔ·ÂÇØÁÖ¼¼¿ä : ";
+	cin >> num1;
+	cin >> num2;
+
+	Add(num1, num2);
+	cout << endl;
+	Sub(num1, num2);
+	cout << endl;
+	Mul(num1, num2);
+	cout << endl;
+	Divide(num1, num2);
+
+
+
+
+	
+	return 0;
+}
