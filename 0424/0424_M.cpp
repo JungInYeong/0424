a67fe8bb90f9273eae3e83e0f3664e38974b788d
JungInYeong <167291569+JungInYeong@users.noreply.github.com>
2024-04-24 14:55:19 +0900

    Update 0424_M.cpp
    연습

--- 0424/0424_M.cpp
+++ 0424/0424_M.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 using namespace std;
 
-// 함수
+// 함수  *이름을 생각해서 정하기*
+// 리턴타입 함수이름( 인수목록 )
+// { 함수본문 }
+
+int Getten()
+{
+	return 10;
+}
+
+int Sum(int n1, int n2)
+{
+	return n1 + n2;
+}
 
 int main()
 {
-	
+	int ten = Getten();
+	cout << ten << endl;
 
+	int sum = Sum(2,3);
+	cout << sum << endl;
 
 	return 0;
 }
